862c837b1c070447c7fb619ae84e3000c4fd5c77
punnpunnpunn <86233160+punnpunnpunn@users.noreply.github.com>
2025-06-22 01:25:58 +0700

    Add queries file

---
+++ my-app/sanity/lib/queries.ts
@@ -0,0 +1,7 @@
+import {defineQuery} from 'next-sanity'
+
+export const POSTS_QUERY = defineQuery(`*[_type == "post"`)
+
+export const POST_QUERY = defineQuery(`*[_type == "post" && slug.current == $slug][0]{
+  title, body, mainImage, author, publishedAt
+}`)
