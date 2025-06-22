95a6735f73aa50965279860e3070e36e2a805aac
punnpunnpunn <86233160+punnpunnpunn@users.noreply.github.com>
2025-06-23 01:51:40 +0700

    render the blog text body

--- my-app/app/(frontend)/blog/[slug]/page.tsx
+++ my-app/app/(frontend)/blog/[slug]/page.tsx
@@ -1,6 +1,9 @@
 import { sanityFetch } from "@/sanity/lib/live";
 import { POST_QUERY } from "@/sanity/lib/queries";
+import { PortableText } from "next-sanity";
 import { notFound } from "next/navigation";
+import Image from "next/image";
+import { urlFor } from "@/sanity/lib/image";
 
 export default async function Page({
   params,
@@ -16,9 +19,37 @@ export default async function Page({
     notFound();
   }
 
+  const convertDate = (date: string | null) => {
+    if (date){
+      return new Date(date).toLocaleDateString("en-US", {day: "numeric", month: "long", year: "numeric"}
+      )}
+    else{
+      return null
+    }
+  }
+
   return (
     <section>
-      <h1 className="text-justify ">{post?.title}</h1>
+      <h1 className="text-justify p-0 mt-[70px] mb-[10px] font-medium">{post?.title}</h1>
+      <p>By Punpun <br/> {convertDate(post?.publishedAt)} </p>
+
+      {post?.mainImage ? (
+        <Image
+          src={urlFor(post.mainImage)
+            .width(800)
+            .height(300)
+            .auto("format")
+            .url()}
+          alt={post?.mainImage?.alt || ""}
+          width="800"
+          height="300"
+        />
+      ) : null}
+      {post?.body ? (
+        <div className="text-xl">
+          <PortableText value={post.body}/>
+        </div>
+      ): null}
     </section>
   );
 }
