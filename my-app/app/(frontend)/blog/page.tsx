af99ba3a5cdb6273d7f961190bbe13fb7d85dcea
punnpunnpunn <86233160+punnpunnpunn@users.noreply.github.com>
2025-06-24 23:48:13 +0700

    Blogs page styled

--- my-app/app/(frontend)/blog/page.tsx
+++ my-app/app/(frontend)/blog/page.tsx
@@ -1,7 +1,6 @@
 import { urlFor } from "@/sanity/lib/image"
 import { sanityFetch } from "@/sanity/lib/live"
 import { POSTS_QUERY } from "@/sanity/lib/queries"
-import { authorType } from "@/sanity/schemaTypes/authorType"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -20,27 +19,21 @@ export default async function Blog() {
     <section>
         <h1>My Blog Posts</h1>
         {posts.map((post) => (
-          <div className="py-2" key={post._id}>
-            <Link
-              className="block hover:text-blue-500 text-2xl"
-              href={`/blog/${post?.slug?.current}`}
-            >
-              {post?.title}
-            </Link>
-            
-            By {post.authorName} {convertDate(post?.publishedAt)}
+          <div className="max-w-xs w-full sm:max-w-2xl sm:flex m-auto my-[50px] shadow-lg sm:shadow-none rounded-lg" key={post._id}>
             {post?.mainImage ? (
               <Image
-                src={urlFor(post.mainImage)
-                  .width(800)
-                  .height(300)
-                  .auto("format")
-                  .url()}
+                src={urlFor(post.mainImage).width(800).height(400).auto("format").url()}
                 alt={post?.mainImage?.alt || ""}
-                width="800"
-                height="300"
+                width={800}
+                height={400}
+                className="w-sm rounded-xl"
               />
             ) : null}
+            <div className="p-4 flex flex-col">
+              <Link className="text-gray-900 font-bold text-2xl mb-3 underline" href={`/blog/${post?.slug?.current}`}>{post?.title}</Link>
+              <p className="text-base text-justify mb-1">{post?.description}</p>
+              <p className="text-sm border-t">{convertDate(post?.publishedAt)}</p>
+            </div>
           </div>
         ))}
     </section>
