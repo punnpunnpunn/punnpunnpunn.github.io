5a563f07d7f573eccbc276370c2b9863c055395e
Punpun <punpunp@hotmail.com>
2025-08-02 22:40:22 +0700

    Added favicon

--- my-app/app/(frontend)/components/Navbar.tsx
+++ my-app/app/(frontend)/components/Navbar.tsx
@@ -1,9 +1,15 @@
 import Link from "next/link"
+import icon from "@/app/icon.svg"
 
 const Navbar = () => {
   return (
     <header className="flex justify-between items-center p-4 border-b">
-      <div className="text-2xl font-bold">🗿 Punx2</div>
+      <div className="flex items-center space-x-2">
+        <img src={icon} />
+        <div className="text-2xl font-bold">
+          Punx2
+        </div>
+      </div>
       <nav className="space-x-6">
           <Link className="no-underline" href="/">HOME</Link>
           <Link className="no-underline" href="/blog">BLOG</Link>
