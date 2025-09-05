0c829e922b7444de4d9484ece6d625acb978de83
Punpun <punpunp@hotmail.com>
2025-09-05 12:03:22 -0400

    More metedata

--- my-app/app/(frontend)/layout.tsx
+++ my-app/app/(frontend)/layout.tsx
@@ -6,6 +6,9 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Punpun's Personal Website",
   description: "Hi, My name is Punnawit Payapvattanavong, but you can call me Punpun :). I'm currently studying at the University of Toronto specializing in Mathematical applications in Economics and Finance, majoring in computer science, and minoring in statistics. I love playing chess, singing, acting, running, doing math and making memes. I'm starting this website as a way for me to learn how to make a website and to add my cool projects all into one place.",
+  keywords: ['Punnawit Payapvattanavong', 'Punpun', 'Personal Website', 'Portfolio', 'Blog', 'University of Toronto', 'Mathematical Applications in Economics and Finance', 'Computer Science', 'Statistics'],
+  authors: [{ name: 'Punnawit Payapvattanavong', url: 'https://punx2.vercel.app' }],
+  creator: 'Punnawit Payapvattanavong',
 };
 
 export default function RootLayout({
