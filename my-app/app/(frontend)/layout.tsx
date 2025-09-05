0928fc3b0c6d2bc17bd97eecedd3a3dd26a120ba
Punpun <punpunp@hotmail.com>
2025-09-05 11:07:19 -0400

    Added website description so that maybe search engines will actually pick it up.

--- my-app/app/(frontend)/layout.tsx
+++ my-app/app/(frontend)/layout.tsx
@@ -1,7 +1,12 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { SanityLive } from "@/sanity/lib/live";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Punpun's Personal Website",
+  description: "Hi, My name is Punnawit Payapvattanavong, but you can call me Punpun :). I'm currently studying at the University of Toronto specializing in Mathematical applications in Economics and Finance, majoring in computer science, and minoring in statistics. I love playing chess, singing, acting, running, doing math and making memes. I'm starting this website as a way for me to learn how to make a website and to add my cool projects all into one place.",
+};
 
 export default function RootLayout({
   children,
