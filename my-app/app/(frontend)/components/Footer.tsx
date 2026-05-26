48ca978b7b93d65fcc321fd537d2e009ec1057ce
Punpun <punpunp@hotmail.com>
2026-05-27 03:10:49 +0700

    Added route tracking + supabase

--- my-app/app/(frontend)/components/Footer.tsx
+++ my-app/app/(frontend)/components/Footer.tsx
@@ -1,17 +1,19 @@
+import Link from "next/link"
+
 const Footer = () => {
   return (
     <footer>
         <section>
             <h2>Contact me:</h2>
             <ul>
-                <li>Email: <a href="mailto:punpunp@hotmail.com">punpunp@hotmail.com</a></li>
-                <li>LinkedIn: <a href="https://www.linkedin.com/in/punnawit/">Punnawit Payapvattanavong</a></li>
-                <li>Github: <a href="https://github.com/punnpunnpunn">punnpunnpunn</a></li>
-                <li>Instagram: <a href="https://www.instagram.com/punpun_thetrainer/">@punpun_thetrainer </a></li>
+                <li>Email: <Link href="mailto:punpunp@hotmail.com">punpunp@hotmail.com</Link></li>
+                <li>LinkedIn: <Link href="https://www.linkedin.com/in/punnawit/">Punnawit Payapvattanavong</Link></li>
+                <li>Github: <Link href="https://github.com/punnpunnpunn">punnpunnpunn</Link></li>
+                <li>Instagram: <Link href="https://www.instagram.com/punpun_thetrainer/">@punpun_thetrainer </Link></li>
                 <li>Discord: punnpunnpunn</li>
                 <li>LineID: 15punpun</li>
             </ul>
-            <p><a className="underline" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">Click this link (trust me bro)</a></p>
+            <p><Link className="underline" href="/api/outbound/special-link" target="_blank">Click this link (trust me bro)</Link></p>
         </section>
     </footer>
   )
