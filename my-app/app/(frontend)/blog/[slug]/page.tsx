770453d0c38c9048edf275a805b8cdb3d01aa842
Punpun <punpunp@hotmail.com>
2026-06-03 23:32:11 +0700

    Revalidate blogs every minute

--- my-app/app/(frontend)/blog/[slug]/page.tsx
+++ my-app/app/(frontend)/blog/[slug]/page.tsx
@@ -1,4 +1,4 @@
-export const dynamic = "force-dynamic";
+export const revalidate = 60;
 
 import { sanityFetch } from "@/sanity/lib/live";
 import { POST_QUERY } from "@/sanity/lib/queries";
--- my-app/app/(frontend)/blog/page.tsx
+++ my-app/app/(frontend)/blog/page.tsx
@@ -1,4 +1,4 @@
-export const dynamic = "force-dynamic";
+export const revalidate = 60;
 
 import { urlFor } from "@/sanity/lib/image"
 import { sanityFetch } from "@/sanity/lib/live"
